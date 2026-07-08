92b1552a597d48b775009ebe80c2ca47f0108567
Rhea Laloo <rhealaloo@gmail.com>
2026-07-08 18:20:49 +0000

    chore: bump version to 2.1.0

--- roscoe/__init__.py
+++ roscoe/__init__.py
@@ -3,6 +3,6 @@
 from roscoe.core.agent_result import AgentResult
 from roscoe.core.agent_runner import AgentRunner
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 __all__ = ["AgentRunner", "AgentResult", "__version__"]
